4e349e158adaa824bceef0c9e3d24c4e71ed0bfb
21-manic-004g <21-manic-004g@21-manic-004>
2024-03-21 17:42:27 +0900

    508 514 commit

---
+++ Jungol/src/Q508/Main.java
@@ -0,0 +1,14 @@
+package Q508;
+
+public class Main {
+	public static void main(String[] args) {
+		String A= "item", B="count", C="price";
+	    String D= "pen",  E="note",  F="eraser";
+		System.out.printf("%10s%10s%10s\n",A,B,C);
+		System.out.printf("%10s%10d%10d\n",D,20,100);
+		System.out.printf("%10s%10d%10d\n",E,5,95);
+		System.out.printf("%10s%10d%10d\n",F,110,97);
+	}
+
+}
+
---
+++ Jungol/src/Q514/Main.java
@@ -0,0 +1,19 @@
+package Q514;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	    System.out.print("height =");
+	    int a= sc.nextInt();
+		sc.close();
+		
+		System.out.print("Your height is ");
+		System.out.print(a+"cm.");
+	
+
+		
+		
+	}
+}
